c8be87b3e7f00967d330536511753240aedef2ef
shawnsalt <1099026709@qq.com>
2026-03-13 15:05:36 +0800

    将页面挪动到views目录下，更符合规范

---
+++ src/views/first/components/owner.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex justify-center items-center">
+        <div class="avatar">
+            <el-avatar :src="row.avatar" :size="25" style="background: linear-gradient(304deg, #9d2cec, #e1b7ff)">
+                {{ String(value).slice(0, 1).toUpperCase() }}
+            </el-avatar>
+            <span class="name no-wrap">{{ value }}</span>
+        </div>
+    </div>
+
+</template>
+<script setup lang="ts">
+import { TaskItem } from '@/type/first'
+// 定义组件的 props
+interface Props {
+    value: string | number; // 状态值（如 'success'/'warning'）
+    row: TaskItem,
+    color?: string;
+    map?: Record<string, { label: string; type: string }>;
+    typeDom?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    size: 'small',
+    color: '',
+    typeDom: 'text'
+});
+</script>
+<style scoped>
+.avatar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #f2f3f5;
+    border-radius: 50px;
+
+    .name {
+        padding: 0 0.5rem;
+        max-width: 5rem;
+        text-overflow: ellipsis;
+        overflow-x: hidden;
+    }
+}
+</style>
---
+++ src/views/first/components/tagWithText.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex items-center justify-center">
+        <div class="tagWithText flex items-center justify-center">
+            <div class="tag" :style="{ backgroundColor: color }" type="info">
+                <span>{{ tagText }}</span>
+            </div>
+            <span class="info">{{ text }}</span>
+        </div>
+    </div>
+
+</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+// 定义组件的 props
+interface Props {
+    tagText: string | number;
+    text?: string;
+    row: object,
+    color?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    tagText: '',
+    text: '',
+    row: () => {
+        return {}
+    },
+    color: '',
+});
+
+
+</script>
+<style scoped>
+.tagRadius {
+    .tag {
+        border-radius: 50px;
+    }
+}
+
+.tagWithText {
+    .info {
+        font-size: 1rem;
+        padding-left: 0.4rem;
+        color: #999999;
+    }
+}
+</style>
